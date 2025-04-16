076cf5849fa8e51b6d750019f777e91244f84d23
FTsune <s2021102102@firstasia.edu.ph>
2025-04-16 11:00:46 +0800

    Adjust timeout

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,7 +2,7 @@ import os
 
 bind = "0.0.0.0:" + os.environ.get("PORT", "10000")
 workers = 1  # For ML models, often 1 worker is better
-timeout = 120  # Longer timeout for processing images
+timeout = 300  # Longer timeout for processing images
 threads = 2
 worker_class = "sync"
 loglevel = "info"
